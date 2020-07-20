5bf1744daac4c2087ff81801b7d33504f3d7d515
sebastienR299 <sebastien.a.1990@gmail.com>
2020-07-20 18:35:30 +0200

    :tada: Modification du fichier gitignore et ajout des clés PEM

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:8000/api/";
+// Demande à Node d'injecter la variable d'environnement concernant l'url de l'API depuis le fichier .env
+export const API_URL = process.env.API_URL;
 
 export const CUSTOMERS_API = API_URL + "customers";
 export const INVOICES_API = API_URL + "invoices";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+// Le package "dotenv" permet de lire le fichier .env qui ce trouve à la racine du dossier et de charger les variables d'environnement au sein de node
+require("dotenv").config();
+
 var Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
@@ -69,6 +72,10 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     .enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+
+    Encore.configureDefinePlugin(options => {
+        options["process.env"].API_URL = process.env.API_URL;
+    });
 ;
 
 module.exports = Encore.getWebpackConfig();
